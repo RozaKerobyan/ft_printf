47320fb50dc00d449b146377bf3fec706f6d9b1c
Roza Kerobyan <97788153+RozaKerobyan@users.noreply.github.com>
2025-02-23 19:39:05 +0400

    Add files via upload

--- ft_printf.c
+++ ft_printf.c
@@ -11,7 +11,6 @@
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-#include <stdio.h>
 
 int	ft_convert(va_list args, const char format)
 {
